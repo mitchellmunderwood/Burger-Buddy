10aa99e5e965d2c6bf1527bc86e8deae527cf015
Mitchell Underwood <mitchellmunderwood@gmail.com>
2020-08-14 16:04:25 -0400

    updated function names in orm to comply with build instructions

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,6 @@
 var connection = require("../config/connection.js");
 var orm = {
-    read: function getBurgers(cb) {
+    read: function selectAll(cb) {
         var query = "select * from burgers";
         connection.query(query, function (err, result) {
             if (err) {
@@ -10,25 +10,23 @@ var orm = {
         })
     },
 
-    create: function createBurger(burger_name, cb) {
+    create: function insertOne(burger_name, cb) {
         var query = "INSERT INTO burgers (burger_name) VALUES (?)"
         connection.query(query, [burger_name], function (err, result) {
             if (err) {
                 throw err;
             }
-
             cb(result);
         });
 
     },
 
-    update: function eatBurger(id, cb) {
+    update: function updateOne(id, cb) {
         var query = "UPDATE burgers SET devoured = true WHERE id = ?"
         connection.query(query, [id], function (err, result) {
             if (err) {
                 throw err;
             }
-
             cb(result);
         });
 
