9a4139a5dc0a9a9dcc22460052be0195d18d68f8
Mitchell Underwood <mitchellmunderwood@gmail.com>
2020-08-14 15:37:50 -0400

    Set up schema and seed file to create db for burger

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+USE burgers_db;
+
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Cheeseburger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Hamburger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Bacon Cheesburger');
