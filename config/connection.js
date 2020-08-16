07061684ee97a87de831dd4f2e744c5359c8bf33
Mitchell Underwood <mitchellmunderwood@gmail.com>
2020-08-15 23:04:12 -0400

    small changes

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,26 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection;
+var connection = mysql.createConnection({
+    host: process.env.DB_H_HOST,
+    port: 3306,
+    user: process.env.DB_H_USER,
+    password: process.env.DB_H_PASSWORD,
+    database: process.env.DB_H_DB
+});
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: process.env.DB_PASSWORD,
-        database: "burgers_db"
-    });
-    // connection = mysql.createConnection({
-    //     host: process.env.DB_H_HOST,
-    //     port: 3306,
-    //     user: process.env.DB_H_USER,
-    //     password: process.env.DB_H_PASSWORD,
-    //     database: process.env.DB_H_DB
-    // });
-};
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//     connection = mysql.createConnection({
+//         host: "localhost",
+//         port: 3306,
+//         user: "root",
+//         password: process.env.DB_PASSWORD,
+//         database: "burgers_db"
+//     });
+   
+// };
 
 // Make connection.
 connection.connect(function (err) {
